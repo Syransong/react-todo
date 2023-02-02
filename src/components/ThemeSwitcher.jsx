487d88ca6ddbec1d8e08a961cb9b71e41a655bdb
Karina Song <ksysong@gmail.com>
2023-02-01 22:31:18 -0800

    implement dark and light mode functionality

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,15 +1,50 @@
 import { useState } from 'react';
-
+import { MoonIcon, SunIcon, SwatchIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import styles from './ThemeSwitcher.module.css';
 
 const ThemeSwitcher = () => {
-
+    const [theme, setTheme] = useState('light');
     const [isColourPicking, setIsColourPicking] = useState(false);
+
+    const handleThemeBtn = () => setTheme(theme === "light" ? "dark" : "light");
+    
     return (
         <aside
             className={styles.wrapper}
         >
-        ThemeSwitcher
+            {
+                isColourPicking ? (
+                    <>
+                        <button
+                            className={`btn ${styles.close}`}
+                            aria-label="CLose colour picking mode"
+                            onClick={() => setIsColourPicking(false)}
+                        >
+                            <XMarkIcon />
+                        </button>
+                        <input type="range"/>
+                    </>
+                )
+                : (
+                    <div className={styles.btns}>
+                        <button
+                            className='btn'
+                            aria-label={`Change theme to ${theme === "light" ? "dark" : "light"} mode`}
+                            role="switch"
+                            onClick={handleThemeBtn}
+                        >
+                            {theme === "dark" ? <SunIcon /> : <MoonIcon />}
+                        </button>
+                        <button
+                            className='btn'
+                            aria-label='Enable colour picking mode'
+                            onClick={() => setIsColourPicking(true)}
+                        >
+                            <SwatchIcon />
+                        </button>
+                    </div>
+                )
+            }
         </aside>
     )
 }
